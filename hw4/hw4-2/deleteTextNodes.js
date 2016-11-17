e87f9480c6d3266cc574233f31e9c3ffad74765d
vibecode <rumashkin@gmail.com>
2016-11-17 02:35:24 +0100

    delete text nodes

---
+++ hw4/hw4-2/deleteTextNodes.js
@@ -0,0 +1,19 @@
+/*ДЗ - 2
+
+Создать функцию `deleteTextNodes`
+Эта функция принимает на вход элемент и должна удалить все текстовые узлы внутри указанного элемента.
+Функция может работать не рекурсивно, то есть не заходить внутрь дочерних элементов контейнера.*/
+
+const deleteTextNodes = function(el) {
+  let childNodes = el.childNodes;
+
+  for (node of childNodes) {
+    if (node.nodeType === 3) {
+      el.removeChild(node);
+    }
+  }
+
+  return el;
+}
+
+module.exports = deleteTextNodes;

7be790de5e023e4f11b1e0a3006bb23ab691b0bb
vibecode <rumashkin@gmail.com>
2016-11-17 15:02:50 +0100

    fixed iteration shift when nodes deleted

--- hw4/hw4-2/deleteTextNodes.js
+++ hw4/hw4-2/deleteTextNodes.js
@@ -1,19 +1,23 @@
 /*ДЗ - 2
 
-Создать функцию `deleteTextNodes`
-Эта функция принимает на вход элемент и должна удалить все текстовые узлы внутри указанного элемента.
-Функция может работать не рекурсивно, то есть не заходить внутрь дочерних элементов контейнера.*/
+ Создать функцию `deleteTextNodes`
+ Эта функция принимает на вход элемент и должна удалить все текстовые узлы внутри указанного элемента.
+ Функция может работать не рекурсивно, то есть не заходить внутрь дочерних элементов контейнера.*/
 
-const deleteTextNodes = function(el) {
-  let childNodes = el.childNodes;
+const deleteTextNodes = function (el) {
+    let childNodes = el.childNodes;
+    let i = 0;
 
-  for (let node of childNodes) {
-    if (node.nodeType === 3) {
-      el.removeChild(node);
+    while (i < childNodes.length) {
+
+        if (childNodes[i].nodeType === 3) {
+            el.removeChild(childNodes[i]);
+        } else {
+            i++;
+        }
     }
-  }
 
-  return el;
-}
+    return el;
+};
 
 module.exports = deleteTextNodes;

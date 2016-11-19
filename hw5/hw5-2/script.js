ad4417bff574469792f19b994469b785940e223f
vibecode <rumashkin@gmail.com>
2016-11-19 20:53:58 +0100

    drag and drop random boxes

---
+++ hw5/hw5-2/script.js
@@ -0,0 +1,67 @@
+// fig.style.backgroundColor = getRandomColor(100, 75);
+
+const button = document.getElementById('button');
+const body = document.body;
+
+let thisBox = null,
+    shiftX,
+    shiftY;
+
+button.addEventListener('click', (e) => {
+    if (e.target === button) {
+        makeBox();
+    }
+});
+
+document.addEventListener('mousedown', (e) => {
+    if (e.target.nodeName === 'DIV') {
+        thisBox = e.target;
+
+        shiftX = e.pageX - getCoords(thisBox).left;
+        shiftY = e.pageY - getCoords(thisBox).top;
+    }
+});
+
+document.onmousemove = move;
+document.onmouseup = endMove;
+
+function makeBox() {
+    let newBox = document.createElement('DIV'),
+        height = (Math.random() + 0.1) * 100 + 'px',
+        width = (Math.random() + 0.1) * 100 + 'px',
+        color = getRandomColor(100, 75),
+        top = Math.random() * (window.innerHeight - parseInt(height)) + "px",
+        left = Math.random() * (window.innerWidth - parseInt(width)) + "px";
+
+    newBox.style.height = height;
+    newBox.style.width = width;
+    newBox.style.backgroundColor = color;
+    newBox.style.top = top;
+    newBox.style.left = left;
+    body.appendChild(newBox);
+}
+
+function getRandomColor(s, l) {
+    let h = Math.round(Math.random() * 360);
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+}
+
+function move(e) {
+    if (thisBox !== null) {
+        thisBox.style.left = (e.pageX - shiftX) + 'px';
+        thisBox.style.top = (e.pageY - shiftY) + 'px';
+    }
+}
+
+function endMove() {
+    thisBox = null;
+}

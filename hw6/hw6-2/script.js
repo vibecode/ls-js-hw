e946b2f7a55bcd56d8f1516ffe71908694cb31d8
vibecode <rumashkin@gmail.com>
2016-11-23 19:33:07 +0100

    initial commit

---
+++ hw6/hw6-2/script.js
@@ -0,0 +1,5 @@
+/*
+Загрузить города при помощи AJAX из
+https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+(сервер поддерживает AJAX CORS)
+Отсортировать города по алфавиту и вывести на странице.*/

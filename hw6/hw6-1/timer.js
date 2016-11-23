2d6b54dbfa0b2f064db83c2bab125f52f1ab61c6
vibecode <rumashkin@gmail.com>
2016-11-23 17:35:12 +0100

    fixed indentation

--- hw6/hw6-1/timer.js
+++ hw6/hw6-1/timer.js
@@ -5,20 +5,20 @@
 */
 
 function timer(time) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			let timeSec = time / 1000;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let timeSec = time / 1000;
 
-		    let endingSec = timeSec % 10 == 1 &&
-		    timeSec % 100 != 11 ? 0 : timeSec % 10 >= 2 &&
-		    timeSec % 10 <= 4 &&
-		    (timeSec % 100 < 10 || timeSec % 100 >= 20) ? "секунды" : "секунд";
+      let endingSec = timeSec % 10 == 1 &&
+        timeSec % 100 != 11 ? 0 : timeSec % 10 >= 2 &&
+        timeSec % 10 <= 4 &&
+        (timeSec % 100 < 10 || timeSec % 100 >= 20) ? "секунды" : "секунд";
 
-			let result = `Я вывелась через ${timeSec} ${endingSec}`
+      let result = `Я вывелась через ${timeSec} ${endingSec}`
 
-			resolve(result);
-		}, time);
- 	});
+      resolve(result);
+    }, time);
+  });
 }
 
 timer(5000).then((result) => console.log(result));

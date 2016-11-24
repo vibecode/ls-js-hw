f299d2844bb8c7522084c03f5d14bffdeadf51af
vibecode <rumashkin@gmail.com>
2016-11-24 23:35:32 +0100

    show cities logic

--- hw6/hw6-3/script.js
+++ hw6/hw6-3/script.js
@@ -1,11 +1,13 @@
 /*
-Создать страничку с текстовым полем.
-После загрузки странички, загрузить список городов при помощи AJAX.
-При вводе текста в тестовое поле, выводить под текстовым полем список тех городов,
-в названиях которых есть введенный текст.
-*/
+ Создать страничку с текстовым полем.
+ После загрузки странички, загрузить список городов при помощи AJAX.
+ При вводе текста в тестовое поле, выводить под текстовым полем список тех городов,
+ в названиях которых есть введенный текст.
+ */
 const citiesUrl = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
 const citiesList = document.querySelector('.cities');
+const input = document.getElementById('cityInput');
+let cities;
 
 function sendRequest(url) {
     return new Promise((resolve, reject) => {
@@ -25,7 +27,7 @@ function sendRequest(url) {
 window.addEventListener('load', (e) => {
     sendRequest(citiesUrl)
         .then((result) => {
-            let cities = JSON.parse(result);
+            cities = JSON.parse(result);
 
             cities.sort((a, b) => {
                 let cityA = a.name.toUpperCase();
@@ -35,19 +37,25 @@ window.addEventListener('load', (e) => {
                     return -1;
                 }
                 if (cityA > cityB) {
-                    return  1;
+                    return 1;
                 }
 
                 return 0;
             });
-
-            citiesList.innerHTML = "";
-
-            for (let cityObj of cities) {
-                citiesList.innerHTML += `<li>${cityObj.name}</li>`;
-            }
-
         }, (error) => {
             citiesList.innerHTML = `<li>${error}</li>`;
-            });
+        });
+});
+
+input.addEventListener('input', (e) => {
+    let inputValue = e.target.value.toLowerCase();
+
+    citiesList.innerHTML = '';
+    for (let cityObj of cities) {
+        let city = cityObj.name.toLowerCase();
+
+        if (city.startsWith(inputValue)) {
+            citiesList.innerHTML += `<li>${cityObj.name}</li>`;
+        }
+    }
 });

3ecbf096dea36dc99b39201cb52302212f6a9a6f
vibecode <rumashkin@gmail.com>
2016-11-28 21:37:53 +0100

    checking  parameters

--- hw1/hw1-1/consoleRec.js
+++ hw1/hw1-1/consoleRec.js
@@ -1,4 +1,8 @@
-function consoleRec(arr, i) {
+function consoleRec(arr, i = 0) {
+  if (!(arr instanceof Array)) {
+    throw new Error('не является массивом');
+  }
+
   if (i < arr.length) {
     console.log(arr[i]);
 
